b4979c0ccbe6f8280130628cfd859367ef8bdf0d
Fernando Acevedo <fernando.acevedo@fractalia.es>
2025-06-02 20:43:48 +0200

    Se añade cmpatibilidad react astro - cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,19 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()]
-    },
-
+  output: "server",
+  adapter: cloudflare(),
   integrations: [react()],
-  adapter: cloudflare()
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: {
+        ...(import.meta.env.PROD && {
+          "react-dom/server": "react-dom/server.edge",
+        }),
+      },
+    },
+  },
 });
